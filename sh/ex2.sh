0e4a71cab5541d5bacd1a61bba278f8e8f652b83
João Vítor Sberse <81939995+yoarajota@users.noreply.github.com>
2023-06-18 15:09:40 -0300

    Update ex2.sh

--- sh/ex2.sh
+++ sh/ex2.sh
@@ -1,1 +1,7 @@
-df ./ | cut -d " " -f17;
+percentual=$(df . | awk 'NR==2 {print $5}' | cut -d '%' -f 1)
+
+if [ $percentual -gt 90 ]; then
+  echo "Acima de 90%!"
+else
+  echo "Abaixo de 90%!"
+fi;
