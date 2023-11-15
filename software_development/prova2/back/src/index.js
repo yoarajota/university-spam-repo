021ab1a78ebd7e11a4ad5cd307970fb6c411baff
yoarajota <joaovbscontato@gmail.com>
2023-11-15 17:47:29 -0300

    feat: start mongoose

--- software_development/prova2/back/src/index.js
+++ software_development/prova2/back/src/index.js
@@ -1,13 +1,25 @@
-import express from 'express'
-import router from './router.js'
+import express from "express";
+import router from "./router.js";
+import mongoose from "mongoose";
+
 const app = express();
 
 router(app);
+// Cria uma aplicação Express
+mongoose.connect(process.env.MONGODB_URL, { dbName: "db_IFRS" });
+
+const db = mongoose.connection;
+mongoose.connection.on("error", function (err) {
+  console.log("Erro na conexão Mongoose padrão: " + err);
+});
+//A conexão foi feita com sucesso
+db.once("open", function () {
+  console.log("Estamos conectados no banco de dados!");
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log("👍 - Server Running");
-})
-
+});
